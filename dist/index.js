1f693f44667dc0ea59df1e397aca8195505195b6
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-01-25 21:37:24 +0300

    Complete Unions & Enum

--- dist/index.js
+++ dist/index.js
@@ -19,3 +19,26 @@ let people = [
     ['John', 23],
     ['Jane', 24]
 ];
+// Union is a type that can be any of the types in the union
+let productID = 1;
+productID = '1';
+// Enum is a way to define a set of named constants.
+// There are three types of enums: Numeric, String, Heterogeneous
+// const gibidir fakat bir takim avantajlari vardir okunurluk, performans gibi.
+// Değer verilmediği zaman değerleri sıralı olarak atanır 0, 1, 2, 3 şeklinde.
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Right); // 3
+var Direction2;
+(function (Direction2) {
+    Direction2["Up"] = "Up";
+    Direction2["Down"] = "Down";
+    Direction2["Left"] = "Left";
+    Direction2["Right"] = "Right";
+})(Direction2 || (Direction2 = {}));
+console.log(Direction2.Left); // Left
--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,31 @@ let people: [string, number][] = [
   ['John', 23],
   ['Jane', 24]
 ]
+
+// Union is a type that can be any of the types in the union
+
+let productID: number | string = 1
+productID = '1'
+
+// Enum is a way to define a set of named constants.
+// There are three types of enums: Numeric, String, Heterogeneous
+// const gibidir fakat bir takim avantajlari vardir okunurluk, performans gibi.
+// Değer verilmediği zaman değerleri sıralı olarak atanır 0, 1, 2, 3 şeklinde.
+
+enum Direction1 {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+console.log(Direction1.Right) // 3
+
+enum Direction2 {
+  Up = 'Up',
+  Down = 'Down',
+  Left = 'Left',
+  Right = 'Right'
+}
+
+console.log(Direction2.Left) // Left
