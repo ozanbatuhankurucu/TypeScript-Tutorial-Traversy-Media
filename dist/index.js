e002cbe9883fa847ad0ef221537a765181639009
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-01-25 19:30:44 +0300

    Complete Basic Types

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// tsc --watch (to start compiler)
+// tsc --init (to create tsconfig.json)
+// dist folder means compiled files
+// TypeScript Basic Types are: number, string, boolean, any
+let id = 1;
+let userName = 'John';
+let isLoggedIn = true;
+let message = 'Hello'; // any is a type that can be anything
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// tsc --watch (to start compiler)
+// tsc --init (to create tsconfig.json)
+// dist folder means compiled files
+
+// TypeScript Basic Types are: number, string, boolean, any
+
+let id: number = 1
+let userName: string = 'John'
+let isLoggedIn: boolean = true
+let message: any = 'Hello' // any is a type that can be anything
