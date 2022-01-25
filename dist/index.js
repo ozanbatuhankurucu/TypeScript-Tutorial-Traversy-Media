c8ad2f70d7aedaabf84b94df26c11ff190e449cd
ozanbatuhankurucu <50013838+ozanbatuhankurucu@users.noreply.github.com>
2022-01-25 19:50:50 +0300

    Complete Arrays & Tuples

--- dist/index.js
+++ dist/index.js
@@ -7,3 +7,15 @@ let id = 1;
 let userName = 'John';
 let isLoggedIn = true;
 let message = 'Hello'; // any is a type that can be anything
+// TypeScript Arrays are: Array<T>
+// İki farklı şekildede tanımlayabiliriz.
+let ids = [1, 2, 3];
+let names = ['John', 'Jane'];
+// TypeScript Tuples are arrays with fixed number of elements
+// Tuple
+let person = ['John', 23];
+// Tuple Array
+let people = [
+    ['John', 23],
+    ['Jane', 24]
+];
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,17 @@ let id: number = 1
 let userName: string = 'John'
 let isLoggedIn: boolean = true
 let message: any = 'Hello' // any is a type that can be anything
+
+// TypeScript Arrays are: Array<T>
+// İki farklı şekildede tanımlayabiliriz.
+let ids: Array<number> = [1, 2, 3]
+let names: string[] = ['John', 'Jane']
+
+// TypeScript Tuples are arrays with fixed number of elements
+// Tuple
+let person: [string, number] = ['John', 23]
+// Tuple Array
+let people: [string, number][] = [
+  ['John', 23],
+  ['Jane', 24]
+]
